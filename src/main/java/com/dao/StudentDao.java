eb29a1840c457f18fa3da255662cbe1ac06122b2
lraelc7 <85848663@qq.com>
2018-09-13 11:39:33 +0800

    spring hibernate整合

---
+++ src/main/java/com/dao/StudentDao.java
@@ -0,0 +1,20 @@
+package com.dao;
+
+import com.entity.Main;
+import com.entity.StudentEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDao {
+    @Autowired
+    HibernateTemplate hibernateTemplate;
+    public void get(int id){
+        StudentEntity studentEntity = hibernateTemplate.get(StudentEntity.class, id);
+
+
+    }
+}
---
+++ src/test/java/SpringHibernateTest.java
@@ -0,0 +1,32 @@
+import com.entity.Main;
+import com.entity.StudentEntity;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring.xml"})
+public class SpringHibernateTest {
+    @Autowired
+    HibernateTemplate hibernateTemplate;
+
+    @Test
+    public void testGet(){
+        StudentEntity studentEntity = hibernateTemplate.get(StudentEntity.class,2);
+        System.out.println(studentEntity);
+    }
+    @Test
+    public void update(){
+        StudentEntity studentEntity = hibernateTemplate.get(StudentEntity.class,1);
+         studentEntity.setName("jerry");
+         studentEntity.setAge(15);
+        System.out.println(studentEntity);
+
+    }
+
+}
